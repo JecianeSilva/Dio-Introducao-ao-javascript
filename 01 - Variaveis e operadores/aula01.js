ca049fca29798496d1bdf4a9a56a89e574808939
LINO LOPES <jeciane_amorim@hotmail.com>
2023-05-05 13:40:27 -0300

    lesson 1

---
+++ 01 - Variaveis e operadores/aula01.js
@@ -0,0 +1,86 @@
+
+//_____________________________VARIAVEIS____________________________________
+//--------------------------------------------------------------------------
+
+// Variável ----------------------------------------------------------------
+let variavel = 10;
+var variavel2 = 15;
+variavel = 20;
+
+console.log(variavel);
+console.log(variavel2);
+
+// Constants ----------------------------------------------------------------
+const pi = 3.14;
+
+console.log(pi);
+
+
+//________________________________ OPERADORES ________________________________
+//----------------------------------------------------------------------------
+
+// Operadores aritméticos ----------------------------------------------------
+//----------------------------------------------------------------------------
+
+//Adição
+let a = 10;
+let b = 20;
+let c = a + b;
+console.log(c);
+
+//Subtração
+let d = 10;
+let e = 20;
+let f = d - e;
+console.log(f);
+
+//Multiplicação
+let g = 10;
+let h = 20;
+let i = g * h;
+console.log(i);
+
+//Divisão
+let j = 10;
+let k = 20;
+let l = j / k;
+console.log(l);
+
+//Resto da  div
+let m = 10;
+let n = 20;
+let o = m % n;
+console.log(o);
+
+//Potenciação   
+let p = 10;
+let q = 20;
+let r = p ** q;
+console.log(r); 
+
+// Operadores lógicos 
+// ---------------------------------------------------------------------------
+
+// and
+let la = 10;
+let lb = 20;
+let lc = a && b;
+console.log(lc);
+
+// or  
+let ld = 10;
+let le = 20;
+let lf = ld || le;
+console.log(lf);
+
+// not 
+let lg = 10;
+let lh = 20;
+let li =!lg || lh;
+console.log(li);
+
+// XOR
+let lj = 10;
+let lk = 20;
+let lm = lj ^ lk;
+console.log(lm);
