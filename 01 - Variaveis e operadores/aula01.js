dca630fbc290930386bb36bf32fcd9bb51d28cdd
LINO LOPES <jeciane_amorim@hotmail.com>
2023-05-09 10:48:30 -0300

    update aula01 with anotation

--- 01 - Variaveis e operadores/aula01.js
+++ 01 - Variaveis e operadores/aula01.js
@@ -19,7 +19,7 @@ console.log(pi);
 //________________________________ OPERADORES ________________________________
 //----------------------------------------------------------------------------
 
-// Operadores aritméticos ----------------------------------------------------
+// Operadores aritméticos e de Comparação----------------------------------------------------
 //----------------------------------------------------------------------------
 
 //Adição
@@ -46,17 +46,75 @@ let k = 20;
 let l = j / k;
 console.log(l);
 
-//Resto da  div
+//Potenciação   
 let m = 10;
 let n = 20;
-let o = m % n;
-console.log(o);
+let o = m ** n;
+console.log(o); 
 
-//Potenciação   
+//Resto da  div
 let p = 10;
 let q = 20;
-let r = p ** q;
-console.log(r); 
+let r = p % q;
+console.log(o);
+
+//Increment
+let num1 = 10;
+let result =num1++;
+console.log(result);
+
+//Decrement
+let num2 = 10;
+let result2 = num2--;
+console.log(result2);
+
+// Igual
+let w1 = 10;
+let w2 = 20;
+let res1 = w1 == w1;
+console.log(res1);
+
+// Diferente
+let x1 = 10;
+let x2 = 20;
+let res2 = x1!= x2;
+console.log(res2);
+
+ // Igual estrita
+ let y1 = 10;
+ let y2 = 20;
+ let res3 = y1 === y2;
+ console.log(res3);
+
+ // Diferente estrita
+ let z1 = 10;
+ let z2 = 20;
+ let res4 = z1!==z2;
+ console.log(res4);
+
+ // Maior
+ let r1 = 10;
+ let r2 = 20;
+ let resultR1 = r1 > r2;
+ console.log(resultR1);
+
+ // Menor
+ let s1 = 10;
+ let s2 = 20;
+ let resultS1 = s1 < s2;
+ console.log(resultS1);
+
+ // Maior ou igual
+ let t1 = 10;
+ let t2 = 20;
+ let resultT1 = t1 >= t2;
+ console.log(resultT1);
+
+ // Menor ou igual
+ let u1 = 10;
+ let u2 = 20;
+ let resultU1 = u1 <= u2;
+ console.log(resultU1);
 
 // Operadores lógicos 
 // ---------------------------------------------------------------------------
@@ -79,8 +137,13 @@ let lh = 20;
 let li =!lg || lh;
 console.log(li);
 
-// XOR
-let lj = 10;
-let lk = 20;
-let lm = lj ^ lk;
-console.log(lm);
+// concat
+let lm = 10;
+let lo = 'lm'+lm+'.';
+console.log(lo);
+
+// Operador condicion ternario ----------------------------------------------
+let ma = 10;
+let mb = 20;
+let mc = ma >= mb ? ma : mb;
+console.log(mc);
